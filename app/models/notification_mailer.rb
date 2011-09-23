0815a53e2a7fb1336b857cfcd871ae03ea3d3047
LuceIt <mario.defrutos@luceit.com>
2011-09-23 12:03:48 +0200

    using emission email address to as from field

--- app/models/notification_mailer.rb
+++ app/models/notification_mailer.rb
@@ -2,6 +2,7 @@ class NotificationMailer < ActionMailer::Base
   def notification_hours_mail(report)
     @report = report
     recipients @report.user.mail
+    from Setting.mail_from
     subject "Notificación de horas de proyecto"
     sent_on Time.now
     content_type "text/html"
